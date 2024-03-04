ad126d86386bc805ded76e31f25a3807fca46942
Raden Demas <radendemas21@gmail.com>
2024-03-04 20:56:39 +0700

    new file:   Unit 1/percobaan1.c 	new file:   Unit 1/percobaan2a.c 	new file:   Unit 1/percobaan2b.c 	new file:   Unit 1/percobaan2c.c

---
+++ Unit 1/percobaan2a.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //<- ditambahkan
+
+int main() {
+  printf("Alpro itu mudah!");
+  return 0;
+}
+
+//program error karena tidak ada library stdio.h
+//karena itu adalah standar input dan output dalam bahasa c
---
+++ Unit 1/percobaan2b.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Alpro itu mudah!"); // -> printf(Alpro itu mudah!);
+  return 0;
+}
+
+//program error karena pada printf tidak memiliki tanda kutip
