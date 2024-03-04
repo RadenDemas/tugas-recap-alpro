cd39a5a398febe343e3f9814dfec616496c7cb5a
Raden Demas <radendemas21@gmail.com>
2024-03-04 21:16:31 +0700

    new file:   Unit 1/percobaan6.c 	new file:   Unit 1/percobaan7a.c 	new file:   Unit 1/percobaan7b.c 	new file:   Unit 1/percobaan8.c 	new file:   Unit 1/percobaan9a.c 	new file:   Unit 1/percobaan9b.c

---
+++ Unit 1/percobaan7a.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char iniChar = 'Romi';
+
+int main(){
+    printf("%c", iniChar);
+    return 0;
+}
+
+//char hanya dapat menyimpan satu karakter saja
+//dan karakter yang disimpan merupakan karakter yang terakhir pada kata/kalimat
---
+++ Unit 1/percobaan7b.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char iniString[] = "Romi";
+
+int main(){
+    printf("%s", iniString);
+    return 0;
+}
+
+//pada C tidak memiliki data variabel C
+//oleh karena nya untuk membuat tipe data string menggunakan char berbentuk array
