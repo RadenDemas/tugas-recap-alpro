7a2fc256991fa56a49da78003dec1c30043d2789
Raden Demas <radendemas21@gmail.com>
2024-03-04 21:02:04 +0700

    new file:   Unit 1/percobaan3a.c 	new file:   Unit 1/percobaan3b.c

---
+++ Unit 1/percobaan3a.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  printf("Baris pertama");
+  printf("Baris kedua \n");
+  printf("Baris ketiga");
+  return 0;
+}
+
+//pada baris kelima ditambahkan tanda \n yang artinya enter
+//jadi pada akhir kalimat pada baris kelima nanti akan secara otomatis enter
+//sedangkan pada baris 4 dan 6 tidak akan enter pada terminal
---
+++ Unit 1/percobaan3b.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("Oh my, oh my God \n예상했어 나 \nI was really hoping that he will come through");
+  return 0;
+}\
+
+//jadi pada setiap kalimat akan ada enter pada baris keempat
+//enter terletak disetiap akhir kalimat "Oh my, oh my God" dan "예상했어 나"
